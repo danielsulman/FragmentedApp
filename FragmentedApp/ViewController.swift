8fae470a8c8883a1ee5ece3e6ea4025291a02a78
Daniel <dsulmandealbuquerqu@luc.edu>
2017-04-25 19:43:35 -0300

    Commit inicial de teste

--- FragmentedApp/ViewController.swift
+++ FragmentedApp/ViewController.swift
@@ -14,6 +14,10 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    @IBAction func buttonclicked(_ sender: UIButton){
+        print("Ths is a git tutorial")
+    }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
