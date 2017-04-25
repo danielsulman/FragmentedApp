72663a4ad3c3390b52c00860e7aa58608f737801
Daniel <dsulmandealbuquerqu@luc.edu>
2017-04-25 20:57:30 -0300

    Segemented funcionando para alternar entre login e cadastro

--- FragmentedApp/ViewController.swift
+++ FragmentedApp/ViewController.swift
@@ -10,9 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var secondView: UIView!
+    @IBOutlet weak var firstView: UIView!
+    @IBOutlet weak var segmentedControl: UISegmentedControl!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+
     }
     
     @IBAction func buttonclicked(_ sender: UIButton){
@@ -24,6 +29,19 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func segmented(_ sender: UISegmentedControl) {
+        switch segmentedControl.selectedSegmentIndex{
+        case 0:
+            firstView.isHidden = false
+            secondView.isHidden = true
+        case 1:
+            firstView.isHidden = true
+            secondView.isHidden = false
+            print(firstView)
+        default:
+            break;
+        }
+    }
 
 }
 
